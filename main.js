50939b1b8eee0a642d0e0154ad7c64c316ce7a73
Cheulong Sear <seacheulong@gmail.com>
2020-12-18 16:14:24 +0700

    duration

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var controller = new ScrollMagic.Controller();
 //scene
 var scene1 = new ScrollMagic.Scene({
   triggerElement: "#second",
+  duration: "30%",
   triggerHook: 0.7, //0-1
 })
   .setClassToggle("#second", "fade-in")
